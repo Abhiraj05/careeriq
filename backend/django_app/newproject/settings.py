aca500a31a3c649b939195d0266b35663dc30c26
mayurkhedekar65 <mayurkhedekar646@gmail.com>
2026-03-29 12:46:27 +0530

    xyz

--- backend/django_app/newproject/settings.py
+++ backend/django_app/newproject/settings.py
@@ -153,9 +153,3 @@ STRIPE_PUBLISHABLE_KEY = 'pk_test_placeholder'
 STRIPE_SECRET_KEY = 'sk_test_placeholder'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mydjango65@gmail.com'
-EMAIL_HOST_PASSWORD = 'oxxy jmoe gpau pqcp' 
